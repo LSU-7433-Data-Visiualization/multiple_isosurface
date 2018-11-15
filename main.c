1ae507b68c3899a660ccce216f286c9a468b3445
Shengfeng Li <cs.shengfeng.li@gmail.com>
2018-11-15 12:32:53 -0600

    First commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+
+#include <GL/glut.h>
+
+#endif
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello world!");
+    return 0;
+}
+
